2bdc926a88184cb183ed1bda713c2a141862887d
Devid Bruno <devidbruno@MacBook-Air-de-jose.local>
2022-11-25 01:08:52 -0300

    make

--- resources/views/admin/listusers.blade.php
+++
@@ -1,7 +0,0 @@
-@foreach ( $users as $user )
-    <div class="card" style="width: 20rem;">
-        <div class="card-body">
-          <h6 class="card-subtitle mb-2 text-muted">{{ $user->name }}</h6>
-        </div>
-      </div>
-      @endforeach
